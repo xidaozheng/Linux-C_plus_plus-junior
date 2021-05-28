ac89b13e423515d5f628dce1bf3da0aeb7c3d922
xidaozheng <13184136638@163.com>
2021-05-27 18:20:46 -0700

    sync company's computer

--- 01_hello_cmake/hello.cpp
+++ 01_hello_cmake/hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(int argc, char **argv)
 {
     cout << "this is cmake test!" << endl;
+    cout << "sync company's computer" << endl;
     cout << "hello world"<<endl;
 }
